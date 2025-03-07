b75770092f9d19aaa865eb79ceb3e4328364875c
shankar7979 <shankar7979@gmail.com>
2025-03-07 16:16:24 +0530

    added

--- kochi 2025_2024_dec/angular/app1_@ViewChild/src/app/app.component.ts
+++ kochi 2025_2024_dec/angular/app1_@ViewChild/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {EmployeeComponent} from './employee/employee.component';
 import {ChildComponent} from './child/child.component';
 import {ParentComponent} from './parent/parent.component';
+import {CounterComponent} from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeComponent,ParentComponent, ChildComponent],
+  imports: [RouterOutlet, EmployeeComponent,ParentComponent, ChildComponent,CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
