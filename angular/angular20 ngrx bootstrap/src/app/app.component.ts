4c95967cfa01508234bf873b8c6f93953d9e03d6
shankar7979 <shankar7979@hotmail.com>
2025-09-02 06:57:23 +0530

    added

--- angular/angular20 ngrx bootstrap/src/app/app.component.ts
+++ angular/angular20 ngrx bootstrap/src/app/app.component.ts
@@ -4,10 +4,12 @@ import { Counterapp1Component } from './counterapp1/counterapp1.component';
 import { CommonModule } from '@angular/common';
 import { Parent1Component } from "./parentchild1/parent1/parent1.component";
 import { Parent2Component } from "./parentchild2/parent2/parent2.component";
+import { BookComponent } from './child-parent-bookhouse/book/book.component';
+import { CustomerComponent } from './child-parent-bookhouse/customer/customer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, Counterapp1Component, Parent1Component, Parent2Component],
+  imports: [RouterOutlet, CommonModule, Counterapp1Component, Parent1Component, Parent2Component, BookComponent,CustomerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
